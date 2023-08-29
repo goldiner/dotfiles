073bf4d0924e2244df81c7a0196750491632d0be
Uri Goldiner <uree.goldiner@gmail.com>
2023-08-29 14:40:41 +0300

    Fixing filters

--- nvim/lua/custom/configs/overrides.lua
+++ nvim/lua/custom/configs/overrides.lua
@@ -1,60 +1,61 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "vim",
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "c",
-    "markdown",
-    "markdown_inline",
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"c",
+		"markdown",
+		"markdown_inline",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "deno",
-    "prettier",
-    "eslint",
-    -- "vscode-eslint-language-server"
-  },
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"deno",
+		"prettier",
+		"eslint",
+		-- "vscode-eslint-language-server"
+	},
 }
 
 -- git support in nvimtree
 M.nvimtree = {
 
-  git = {
-    enable = true,
-  },
-
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	git = {
+		enable = true,
+		ignore = false,
+		timeout = 10000,
+	},
+	filters = {
+		dotfiles = false,
+		-- custom = { "^.git$" },
+	},
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
-
-
-
-
 
 return M
