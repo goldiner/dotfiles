a07849c0279a68d5a56707e4fa674f28de63363a
Uri Goldiner <uree.goldiner@gmail.com>
2023-09-28 21:46:38 +0300

    More configurations

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,176 +1,176 @@
-local overrides = require "custom.configs.overrides"
+local overrides = require("custom.configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  {
-    "kevinhwang91/nvim-bqf",
-    lazy = false,
-    -- config = function()
-    --   require("bqf").setup() {
-    --     -- ft = "qf",
-    --     auto_enable = true,
-    --   }
-    -- end,
-  },
+	{
+		"kevinhwang91/nvim-bqf",
+		lazy = false,
+		-- config = function()
+		--   require("bqf").setup() {
+		--     -- ft = "qf",
+		--     auto_enable = true,
+		--   }
+		-- end,
+	},
 
-  {
-    "tpope/vim-fugitive",
-    lazy = false,
-    -- cmd = "Git"
-    -- config = function()
-    --   require("vim-fugitive").setup(){
-    --     keys = {}
-    --   }
-    -- end,
-  },
-  {
-    "vimwiki/vimwiki",
-    lazy = false,
-    -- config = function()
-    --   require("vimwiki").setup() {}
-    --   -- vim.g.vimwiki_list = {
-    --   --   {
-    --   --     path = '/home/urig/vimwiki',
-    --   --     syntax = 'markdown',
-    --   --     ext = '.md',
-    --   --   }
-    --   -- }
-    --   -- }
-    -- end,
-    -- -- cmd = "Wiki"
-  },
-  {
-    "phaazon/hop.nvim",
-    lazy = false,
-    config = function()
-      require("hop").setup()
-    end,
-  },
-  {
-    "rmagatti/auto-session",
-    lazy = false,
-    config = function()
-      require("auto-session").setup {
-        log_level = "error",
-        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-      }
-    end,
-  },
-  -- 'kevinhwang91/nvim-bqf'
-  -- {
-  --   "folke/trouble.nvim",
-  --   requires = "nvim-tree/nvim-web-devicons",
-  --   config = function()
-  --     require("trouble").setup {
-  --       -- your configuration comes here
-  --       -- or leave it empty to use the default settings
-  --       -- refer to the configuration section below
-  --       -- auto_preview = false,
-  --       -- mode = "quickfix",
-  --       -- auto_jump = {}
-  --     }
-  --   end
-  -- },
+	{
+		"tpope/vim-fugitive",
+		lazy = false,
+		-- cmd = "Git"
+		-- config = function()
+		--   require("vim-fugitive").setup(){
+		--     keys = {}
+		--   }
+		-- end,
+	},
+	{
+		"vimwiki/vimwiki",
+		lazy = false,
+		-- config = function()
+		--   require("vimwiki").setup() {}
+		--   -- vim.g.vimwiki_list = {
+		--   --   {
+		--   --     path = '/home/urig/vimwiki',
+		--   --     syntax = 'markdown',
+		--   --     ext = '.md',
+		--   --   }
+		--   -- }
+		--   -- }
+		-- end,
+		-- -- cmd = "Wiki"
+	},
+	{
+		"phaazon/hop.nvim",
+		lazy = false,
+		config = function()
+			require("hop").setup()
+		end,
+	},
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+		config = function()
+			require("auto-session").setup({
+				log_level = "error",
+				auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			})
+		end,
+	},
+	-- 'kevinhwang91/nvim-bqf'
+	-- {
+	--   "folke/trouble.nvim",
+	--   requires = "nvim-tree/nvim-web-devicons",
+	--   config = function()
+	--     require("trouble").setup {
+	--       -- your configuration comes here
+	--       -- or leave it empty to use the default settings
+	--       -- refer to the configuration section below
+	--       -- auto_preview = false,
+	--       -- mode = "quickfix",
+	--       -- auto_jump = {}
+	--     }
+	--   end
+	-- },
 
-  -- {
-  --    "dmmulroy/tsc.nvim",
-  -- config = function()
-  --   require("tsc").setup {
-  --     auto_open_qflist = true,
-  --     enable_progress_notifications = true,
-  --     flags = "-b",
-  --     hide_progress_notifications_from_history = true,
-  --     spinner = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" },
-  --   }
-  -- end
-  -- },
+	-- {
+	--    "dmmulroy/tsc.nvim",
+	-- config = function()
+	--   require("tsc").setup {
+	--     auto_open_qflist = true,
+	--     enable_progress_notifications = true,
+	--     flags = "-b",
+	--     hide_progress_notifications_from_history = true,
+	--     spinner = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" },
+	--   }
+	-- end
+	-- },
 
-  -- 'dmmulroy/tsc.nvim'
-  --   {
-  --     "MunifTanjim/eslint.nvim",
-  --     config = function()
-  -- require("eslint").setup({
-  --   bin = 'eslint_d', -- or `eslint_d`
-  --   code_actions = {
-  --     enable = true,
-  --     apply_on_save = {
-  --       enable = true,
-  --       types = { "directive", "problem", "suggestion", "layout" },
-  --     },
-  --     disable_rule_comment = {
-  --       enable = true,
-  --       location = "separate_line", -- or `same_line`
-  --     },
-  --   },
-  --   diagnostics = {
-  --     enable = true,
-  --     report_unused_disable_directives = false,
-  --     run_on = "type", -- or `save`
-  --   },
-  -- })
-  --     end,
-  --   },
+	-- 'dmmulroy/tsc.nvim'
+	--   {
+	--     "MunifTanjim/eslint.nvim",
+	--     config = function()
+	-- require("eslint").setup({
+	--   bin = 'eslint_d', -- or `eslint_d`
+	--   code_actions = {
+	--     enable = true,
+	--     apply_on_save = {
+	--       enable = true,
+	--       types = { "directive", "problem", "suggestion", "layout" },
+	--     },
+	--     disable_rule_comment = {
+	--       enable = true,
+	--       location = "separate_line", -- or `same_line`
+	--     },
+	--   },
+	--   diagnostics = {
+	--     enable = true,
+	--     report_unused_disable_directives = false,
+	--     run_on = "type", -- or `save`
+	--   },
+	-- })
+	--     end,
+	--   },
 
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
 
-  -- To use a extras plugin
-  -- { import = "custom.configs.extras.symbols-outline", },
+	-- To use a extras plugin
+	-- { import = "custom.configs.extras.symbols-outline", },
 }
 vim.g.vimwiki_list = {
-  {
-    path = "/home/urig/vimwiki",
-    syntax = "markdown",
-    ext = ".md",
-  },
+	{
+		path = "/home/uri/vimwiki",
+		syntax = "markdown",
+		ext = ".md",
+	},
 }
 vim.g.vimwiki_ext2syntax = { [".md"] = "markdown", [".markdown"] = "markdown", [".mdown"] = "markdown" }
 return plugins
